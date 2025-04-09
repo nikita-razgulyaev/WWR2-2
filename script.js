f2b066f017ca31722329f484d778a348c717982e
nikita-razgulyaev <razgulaevnikita406@gmail.com>
2025-04-09 21:39:54 +0300

    Commit message

---
+++ script.js
@@ -0,0 +1,262 @@
+function adaptPage() {
+  // Получаем ширину окна
+  const windowWidth = window.innerWidth;
+
+  // Вычисляем коэффициент масштабирования
+  const zoomLevel = windowWidth / 14.4;
+
+  // Находим элемент main
+  const mainElement = document.querySelector('main');
+
+  // Применяем свойство zoom
+  if (mainElement) {
+      mainElement.style.zoom = zoomLevel + '%';
+  }
+}
+
+// Вызываем функцию при загрузке страницы
+window.addEventListener('load', adaptPage);
+
+// Вызываем функцию при изменении размера окна
+window.addEventListener('resize', adaptPage);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--1');
+  const tooltipText = document.querySelector('.tooltiptext.--1');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--2');
+  const tooltipText = document.querySelector('.tooltiptext.--2');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--3');
+  const tooltipText = document.querySelector('.tooltiptext.--3');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--4');
+  const tooltipText = document.querySelector('.tooltiptext.--4');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--5');
+  const tooltipText = document.querySelector('.tooltiptext.--5');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--6');
+  const tooltipText = document.querySelector('.tooltiptext.--6');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--7');
+  const tooltipText = document.querySelector('.tooltiptext.--7');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--8');
+  const tooltipText = document.querySelector('.tooltiptext.--8');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--9');
+  const tooltipText = document.querySelector('.tooltiptext.--9');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--10');
+  const tooltipText = document.querySelector('.tooltiptext.--10');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--11');
+  const tooltipText = document.querySelector('.tooltiptext.--11');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--12');
+  const tooltipText = document.querySelector('.tooltiptext.--12');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--13');
+  const tooltipText = document.querySelector('.tooltiptext.--13');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--14');
+  const tooltipText = document.querySelector('.tooltiptext.--14');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--15');
+  const tooltipText = document.querySelector('.tooltiptext.--15');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltip = document.querySelector('.tooltip.--16');
+  const tooltipText = document.querySelector('.tooltiptext.--16');
+
+  tooltip.addEventListener('mouseenter', function() {
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.opacity = '1';
+  });
+
+  tooltip.addEventListener('mouseleave', function() {
+    tooltipText.style.visibility = 'hidden';
+    tooltipText.style.opacity = '0';
+  });
+});
